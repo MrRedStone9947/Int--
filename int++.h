d9896ef752c654db641277c61d48bda312bd84a3
Mr_RedStone <118877217+MrRedStone9947@users.noreply.github.com>
2023-05-07 21:36:20 +0800

    Update int++.h

--- int++.h
+++ int++.h
@@ -20,47 +20,38 @@ class BigInt{
 		}
 		void init(string x){
 			v=vector<char>();
-			size=x.size();
 			char st='0';
 			int r=0;
 			if(x[0]=='-'){
+				if(x.size()<2){
+					init(BigInt());
+					return;
+				}
 				r=1;
-				size--;
 				if(x[1]!='0'){
 					st='1';
 				}
 			}
 			v.push_back(st);
-			for(int i=x.size()-1;i>=r;i--){
+			int t=r;
+			for(;t<x.size()-1;t++){
+				if(x[t]!='0'){
+					break;
+				}
+			}
+			x=x.substr(t);
+			size=x.size();
+			for(int i=x.size()-1;i>=0;i--){
+				if(x[i]<'0'||x[i]>'9'){
+					init();
+					return;
+				}
 				v.push_back(x[i]);
 			}
 		}
-		void init(long long x){
-			v=vector<char>();
-			char st='0';
-			if(x<0){
-				st='1';
-			}
-			v.push_back(st);
-			size=0;
-			if(x==0){
-				v.push_back('0');
-				size=1;
-				return;
-			}
-			x=abs(x);
-			while(x){
-				size++;
-				v.push_back(x%10+'0');
-				x/=10;
-			}
-		}
 		void init(BigInt x){
-			v=vector<char>();
 			size=x.size;
-			for(int i=0;i<=size;i++){
-				v.push_back(x.v[i]);
-			}
+			v=x.v;
 		}
 		
 		bool isEqual(BigInt x){
@@ -228,9 +219,12 @@ class BigInt{
 				ret.size++;
 				ret.v.push_back('0');
 			}
-			BigInt t(v[size]-'0');
-			for(int i=size-1;i>size-x.size+1;i--){
-				t=t*10+(v[i]-'0');
+			BigInt t;
+			for(int i=size;i>size-x.size+1;i--){
+				t=t+(v[i]-'0');
+				if(i>size-x.size+2){
+					t*=10;
+				}
 			}
 			for(int i=size-x.size+1;i>=1;i--){
 				t=t*10+(v[i]-'0');
@@ -290,7 +284,7 @@ class BigInt{
 			init(x);
 		}
 		BigInt(long long x){
-			init(x);
+			init(to_string(x));
 		}
 		BigInt operator=(string x){
 			init(x);
@@ -309,6 +303,12 @@ class BigInt{
 		}
 		bool operator==(long long x){
 			return *this==BigInt(x);
+		}
+		bool operator!=(BigInt x){
+			return !(*this==x);
+		}
+		bool operator!=(long long x){
+			return *this!=BigInt(x);
 		}
 		bool operator<(BigInt x){
 			return isSmaller(x);
@@ -441,11 +441,221 @@ class BigInt{
 			return ret;
 		}
 };
+
+class BigFloat{
+	private:
+		BigInt intv;
+		vector<char> floatv;
+		int float_size;
+		int output_count;
+		bool neg;
+		
+		void init(){
+			output_count=1;
+			neg=0;
+			intv=BigInt();
+			floatv=vector<char>();
+			float_size=1;
+			floatv.push_back('0');
+			floatv.push_back('0');
+		}
+		void init(string x){
+			neg=0;
+			intv=BigInt();
+			floatv=vector<char>();
+			floatv.push_back('0');
+			int st=(x[0]=='-');
+			bool f=0;
+			for(int i=st;i<x.size();i++){
+				if(x[i]=='.'){
+					f=1;
+					intv=BigInt(x.substr(st,i-st));
+					float_size=x.size()-i-1;
+					for(int j=i+1;j<x.size();j++){
+						floatv.push_back(x[j]);
+					}
+					while(floatv[float_size]=='0'&&float_size>1){
+						float_size--;
+						floatv.pop_back();
+					}
+					break;
+				}
+				else if(x[i]<'0'||x[i]>'9'){
+					init();
+					return;
+				}
+			}
+			if(!f){
+				intv=BigInt(x.substr(st));
+				float_size=1;
+				floatv.push_back('0');
+			}
+			if(x[0]=='-'){
+				if(!(intv==0&&floatv[1]=='0'&&float_size==1)){
+					neg=1;
+				}
+			}
+			output_count=float_size;
+		}
+		void init(BigFloat x){
+			float_size=x.float_size;
+			output_count=x.output_count;
+			neg=x.neg;
+			intv=x.intv;
+			floatv=x.floatv;
+		}
+		
+		bool isEqual(BigFloat x){
+			if(float_size!=x.float_size||neg!=x.neg){
+				return 0;
+			}
+			if(intv!=x.intv){
+				return 0;
+			}
+			for(int i=1;i<=float_size;i++){
+				if(floatv[i]!=x.floatv[i]){
+					return 0;
+				}
+			}
+			return 1;
+		}
+		bool isSmaller(BigFloat x){
+			if(neg==0&&x.neg==1){
+				return 0;
+			}
+			if(neg==1&&x.neg==0){
+				return 1;
+			}
+			if(intv!=x.intv){
+				return intv<x.intv;
+			}
+			for(int i=1;i<=max(float_size,x.float_size);i++){
+				int a=0,b=0;
+				if(i<=float_size){
+					a=floatv[i]-'0';
+				}
+				if(i<=x.float_size){
+					b=x.floatv[i]-'0';
+				}
+				if(a>b){
+					return 0;
+				}
+				if(a<b){
+					return 1;
+				}
+			}
+			return 0;
+		}
+		bool isBigger(BigFloat x){
+			if(neg==0&&x.neg==1){
+				return 1;
+			}
+			if(neg==1&&x.neg==0){
+				return 0;
+			}
+			if(intv!=x.intv){
+				return intv>x.intv;
+			}
+			for(int i=1;i<=max(float_size,x.float_size);i++){
+				int a=0,b=0;
+				if(i<=float_size){
+					a=floatv[i]-'0';
+				}
+				if(i<=x.float_size){
+					b=x.floatv[i]-'0';
+				}
+				if(a>b){
+					return 1;
+				}
+				if(a<b){
+					return 0;
+				}
+			}
+			return 0;
+		}
+		
+		void input(){
+			string s;
+			cin>>s;
+			init(s);
+		}
+		void print(){
+			if(neg){
+				printf("-");
+			}
+			cout<<intv;
+			if(output_count>0){
+				printf(".");
+				for(int i=1;i<=output_count;i++){
+					if(i<=float_size){
+						printf("%c",floatv[i]);
+					}
+					else{
+						printf("0");
+					}
+				}
+			}
+		}
+	public:
+		BigFloat(){
+			init();
+		}
+		BigFloat(string x){
+			init(x);
+		}
+		
+		bool operator==(BigFloat x){
+			return isEqual(x);
+		}
+		bool operator!=(BigFloat x){
+			return !(*this==x);
+		}
+		bool operator<(BigFloat x){
+			return isSmaller(x);
+		}
+		bool operator>(BigFloat x){
+			return isBigger(x);
+		}
+		bool operator<=(BigFloat x){
+			return *this<x||*this==x;
+		}
+		bool operator>=(BigFloat x){
+			return *this>x||*this==x;
+		}
+		
+		friend ostream& operator<<(ostream& os,BigFloat x);
+		friend istream& operator>>(istream& os,BigFloat& x);
+		
+		int floatLength(){
+			return float_size;
+		}
+		int intLength(){
+			return intv.length();
+		}
+		int length(){
+			return intLength()+floatLength();
+		}
+		void showFloat(){
+			output_count=float_size;
+		}
+		void showFloat(int x){
+			output_count=x;
+		}
+};
+
 ostream& operator<<(ostream& os,BigInt x){
 	x.print();
 	return os;
 }
 istream& operator>>(istream& os,BigInt& x){
+	x.input();
+	return os;
+}
+ostream& operator<<(ostream& os,BigFloat x){
+	x.print();
+	return os;
+}
+istream& operator>>(istream& os,BigFloat& x){
 	x.input();
 	return os;
 }
